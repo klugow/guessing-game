c95d8ab21f586d5ccad929dfd1d1d5a0dce94afc
Andrey Makarenko <119301983+klugow@users.noreply.github.com>
2023-02-21 23:17:10 +0300

    js code added

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
-
+constructor() {
     }
+    setRange(min, max) {
+this.min = min;
+this.max = max;
+    };
 
     guess() {
-
+this.try = Math.round((this.max - this.min) / 2) 
++ this.min;
+return this.try;
     }
 
     lower() {
-
+this.max = this.try;
     }
 
     greater() {
-
+this.min = this.try;
     }
-}
+    }
 
 module.exports = GuessingGame;
